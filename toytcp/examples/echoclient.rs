782d43a426abcd46e079adbd22f9996a747aebae
zeek0x <0h23k1.n40k1@gmail.com>
2022-01-10 03:40:45 +0900

    3.7.7: Operation check.

--- toytcp/examples/echoclient.rs
+++ toytcp/examples/echoclient.rs
@@ -17,6 +17,8 @@ fn echo_client(remote_addr: Ipv4Addr, remote_port: u16) -> Result<()> {
     loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
-        tcp.send(sock_id, input.as_bytes())?;
+        loop {
+            tcp.send(sock_id, input.repeat(2000).as_bytes())?;
+        }
     }
 }
